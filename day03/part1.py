96bf21fb6807c567828a524f1bea0fadf9474bf9
Tim Mackay <simplytim42@gmail.com>
2023-02-28 21:37:35 +0000

    update part 1

--- day03/part1.py
+++ day03/part1.py
@@ -1,4 +1,4 @@
-from rucksack import Rucksack
+from rucksack import Rucksack, getValueOfItem
 from pathlib import Path
 
 sum_total = 0
@@ -6,8 +6,8 @@ sum_total = 0
 with Path("./input.txt").open('r') as f:
     line = f.readline()
     while line:
-        r = Rucksack(line.strip())
-        sum_total = sum_total + r.getValueOfItem()
+        rucksack = Rucksack(line.strip())
+        sum_total = sum_total + getValueOfItem(rucksack.itemInBothCompartments.pop())
         line = f.readline()
 
 print('part 1 sum total: ', sum_total)
