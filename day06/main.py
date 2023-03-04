640df5ce1924e4abebba698a26f2f3e4d00687d3
Tim Mackay <simplytim42@gmail.com>
2023-03-04 14:01:48 +0000

    day6 part2

--- day06/main.py
+++ day06/main.py
@@ -1,26 +1,40 @@
 from pathlib import Path
 
-class PacketMarkerFinder:
-    def __init__(self) -> None:
+class UniqueCombinationFinder:
+    def __init__(self, uniqueAmount: int) -> None:
         self.characters = []
+        self.uniqueAmount = uniqueAmount
 
     def addChar(self, char: str) -> None:
-        if len(self.characters) > 3:
+        if len(self.characters) >= self.uniqueAmount:
             self.characters.pop(0)
         self.characters.append(char)
 
     def isPacketMarker(self) -> bool:
-        if len(self.characters) < 4:
+        if len(self.characters) < self.uniqueAmount:
             return False
-        return True if len(set(self.characters)) == 4 else False
+        return True if len(set(self.characters)) == self.uniqueAmount else False
 
+# part 1
 with Path('./datastream.txt').open() as file:
     data = file.readline()
-    markerFinder = PacketMarkerFinder()
+    packetMarkerFinder = UniqueCombinationFinder(4)
     count = 1
     for char in data:
-        markerFinder.addChar(char)
-        if markerFinder.isPacketMarker():
+        packetMarkerFinder.addChar(char)
+        if packetMarkerFinder.isPacketMarker():
             print("part 1 answer: ", count)
+            break
+        count = count + 1
+
+#  part 2
+with Path('./datastream.txt').open() as file:
+    data = file.readline()
+    messageMarkerFinder = UniqueCombinationFinder(14)
+    count = 1
+    for char in data:
+        messageMarkerFinder.addChar(char)
+        if messageMarkerFinder.isPacketMarker():
+            print("part 2 answer: ", count)
             break
         count = count + 1
