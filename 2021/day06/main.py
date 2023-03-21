abb10d959a0bcfe9d2d67a09200f4eda8c561d74
Tim Mackay <simplytim42@gmail.com>
2023-03-21 23:35:36 +0000

    day 6 part 2

--- 2021/day06/main.py
+++ 2021/day06/main.py
@@ -9,14 +9,43 @@ def add_a_day(list_of_fish: list):
     
     for i in range(amount_to_append):
         list_of_fish.append(8)
-
     return list_of_fish
 
-with open('./input.txt') as file:
-    list_of_fish = [int(fish) for fish in file.readline().split(',')]
+# with open('./input.txt') as file:
+#     list_of_fish = [int(fish) for fish in file.readline().strip().split(',')]
+#     for i in range(80):
+#         list_of_fish = add_a_day(list_of_fish)
+#     print('part1: ', len(list_of_fish))
 
-    for i in range(80):
-        list_of_fish = add_a_day(list_of_fish)
-    
-    print('part1: ', len(list_of_fish))
-    
+
+with open('./input.txt') as file:
+    list_of_fish = [int(fish) for fish in file.readline().strip().split(',')]
+    tally = {}
+    for fish in list_of_fish:
+        if fish not in tally:
+            tally[fish] = 1
+        else:
+            tally[fish] += 1
+
+    total_fish_by_age = {}
+    for day in range(256):
+        temp_tally = {}
+        for age, count in tally.items():
+            if age == 0:
+                if 6 not in temp_tally:
+                    temp_tally.update({6:0})
+                age_6_count = temp_tally[6]
+                temp_tally.update({6:age_6_count + count})
+
+                if 8 not in temp_tally:
+                    temp_tally.update({8:0})
+                age_8_count = temp_tally[8]
+                temp_tally.update({8:age_8_count + count})
+            else:
+                if age-1 not in temp_tally:
+                    temp_tally.update({age-1:0})
+
+                age_count = temp_tally[age-1]
+                temp_tally.update({age-1:age_count + count})
+        tally = temp_tally
+print("part 2:",sum(tally.values()))
